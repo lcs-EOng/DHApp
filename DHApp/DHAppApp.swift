aeef5d4478386e175c63a70880d65656b2d578b6
Eason Ong <eong@lcs.on.ca>
2024-06-06 09:36:12 -0400

    Created a view for the history of menus

--- DHApp/DHAppApp.swift
+++ DHApp/DHAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct interactiveAppsApp: App {
     var body: some Scene {
         WindowGroup {
-            TabView_TabItems()
+            LoginView()
         }
         }
     }
